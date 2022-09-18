7d84cc6a437b21af7564765175f0382f8dd17005
gauthamrajkumarg <gauthamrajkumarg@gmail.com>
2022-09-18 16:03:19 +0700

    initial

--- NeighborlyAPI/getPost/__init__.py
+++ NeighborlyAPI/getPost/__init__.py
@@ -3,16 +3,16 @@ import pymongo
 import json
 from bson.json_util import dumps
 from bson.objectid import ObjectId
-
+import os
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     id = req.params.get('id')
 
     if id:
         try:
-            url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+            url = os.environ["MyDbConnection"]  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
-            database = client['azure']
+            database = client['neighbourlyAppDB']
             collection = database['posts']
 
             query = {'_id': ObjectId(id)}
